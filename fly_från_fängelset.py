3018ec00fa7fc9fa1eef5fea0c370fd53a7ba836
Marcus Malinda Lindmark <supermarcus.lindmark@gmail.com>
2024-11-23 11:10:13 +0100

    Gjorde så man kan gå tillbaka i rummen med hjälp av dictionaries.

--- fly_från_fängelset.py
+++ fly_från_fängelset.py
@@ -1,8 +1,7 @@
 
 # ***** VARIABLER I SPELET *****
-spelet_körs = True
-aktuellt_rum = "rum_1"  # Håller reda på i vilket rum spelaren befinner sig i
-
+spelet_körs = True                  # Används i spelets huvudloop och är True om spelet ska fortsätta köras
+aktuellt_rum = "rum_1"              # Håller reda på i vilket rum spelaren befinner sig i
 
 # ***** DICTIONARIES I SPELET *****
 
@@ -60,15 +59,34 @@ rum = {
     }   
 }
 
+# Dictionary som håller reda på om spelaren har besökt rummet tidigare
+varit_i_rummet = {
+    "rum_1": False,
+    "rum_2": False,
+    "rum_3": False,
+    "rum_4": False,
+    "rum_5": False,
+}
+
 
 # ***** FUNKTIONER I SPELET *****
 
 # RUM 1
 def rum_1(aktuellt_rum):
     spelaren_väljer = True
+
     while (spelaren_väljer):
-        print(f"Du befinner dig i {rum["rum_1"]["beskrivning"]}")
-        print ("Det luktar unket här och du förstår att någon eller något har tagit dig till fånga.")
+        
+        # Beskrivningen av rummet ändras om spelaren har varit där tidigare
+        if varit_i_rummet["rum_1"]:
+            print("\nDu återvänder till det smutsiga och kala rummet utan fönster.\n")
+        else:
+            print(f"\nDu befinner dig i {rum['rum_1']['beskrivning']}")
+            print ("Det luktar unket här och du förstår att någon eller något har tagit dig till fånga.")
+        
+        # Markera att spelaren har varit i rummet
+        varit_i_rummet["rum_1"] = True
+
         print ("1. Öppna dörren.")
         print ("2. Genomsöka rummet.")
         spelarens_val = input("Vad vill du göra? ")
@@ -80,8 +98,8 @@ def rum_1(aktuellt_rum):
                 # Avslutar while-loopen
                 spelaren_väljer = False 
                 
-                # Byt till rum_2
-                return "rum_2"  
+                # Byt till rum_2, genom att returnera ett nytt värde till aktuellt_rum i spelloopen
+                return "rum_2"
             else:
                 print("\nDörren är låst. Du behöver en nyckel för att öppna den.\n")
         elif (spelarens_val == "2"):
@@ -103,17 +121,30 @@ def rum_1(aktuellt_rum):
 def rum_2(aktuellt_rum): 
     spelaren_väljer = True
     while (spelaren_väljer):
-        print(f"Du befinner dig i {rum["rum_2"]["beskrivning"]}")
-        print ("Du vill inte gå bakåt därifrån du kom. Då återstår följande val.")
+        # Beskrivningen av rummet ändras om spelaren har varit där tidigare
+        if varit_i_rummet["rum_2"]:
+            print("\nÅterigen är du i det mörka och fuktiga rummet med märkena på väggarna.\n")
+        else:
+            print(f"Du kommer in i {rum["rum_2"]["beskrivning"]}")
+            
+        # Markera att spelaren har varit i rummet
+        varit_i_rummet["rum_2"] = True
+        
+        print ("Du kan gå:")
         print ("1. Gå höger.")
         print ("2. Gå vänster")
-        print ("3. Genomsöka rummet.")
+        print ("3. Bakåt till cellen igen.")
+        print ("\nDu kan också:")
+        print ("4. Genomsöka rummet.")
         spelarens_val = input("Vad vill du göra? ")
         if (spelarens_val == "1"):
             print("\nDu går åt höger.\n")
         elif (spelarens_val == "2"):
             print ("\nDu går åt vänster.\n")
         elif (spelarens_val == "3"):
+            # Byt till rum_1
+            return "rum_1"  
+        elif (spelarens_val == "4"):
             print ("\nDet finns inget att hitta här.\n")
         
 
@@ -121,6 +152,7 @@ def rum_2(aktuellt_rum):
 while (spelet_körs):
     # När spelaren är klar i rummet och går till nästa rum 
     # uppdateras rumvariabeln till den nya platsen  
+  
     if aktuellt_rum == "rum_1":
         aktuellt_rum = rum_1(aktuellt_rum)
     elif aktuellt_rum == "rum_2":
@@ -128,4 +160,5 @@ while (spelet_körs):
     else:
         print("Spelet är slut.")
         break
+
         
